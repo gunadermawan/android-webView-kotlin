1f8b330678bf29453d02b97d17d343a3f3fe03cd
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-08-06 23:31:20 +0700

    commit pertama aplikasi

---
+++ app/src/main/java/com/gunder/webview_kotlin/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.gunder.webview_kotlin
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    private var webView: WebView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        membuat full screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        webView = findViewById(R.id.wV1)
+        webView!!.webViewClient = WebViewClient()
+        webView!!.loadUrl("https://www.kai.id/")
+        var webSettings = webView!!.settings
+        webSettings.javaScriptEnabled = true
+    }
+
+    override fun onBackPressed() {
+        if (webView!!.canGoBack()){
+            webView!!.goBack()
+        } else{
+            super.onBackPressed()
+        }
+    }
+}
